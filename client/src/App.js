6c96502367d7de41d2c7db92b3f70d3a360e93b2
SD_emg <sandiegoemg@gmail.com>
2019-01-31 17:48:20 -0800

    resolving pkgjson and yarnlock

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AuthService from './components/AuthService';
-import withAuth from './components/withAuth';
-const Auth = new AuthService();
 
 class App extends Component {
-
-
-  handleLogout = () => {
-    Auth.logout();
-    this.props.history.replace('/signup');
-  };
-
-  goToEditProfile = () => {
-    this.props.history.replace('/profile');
-  };
 
   render() {
     console.log(process.env.REACT_APP_SECRET_CODE);
@@ -23,15 +10,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome {this.props.user.email}</h2>
         </div>
-        <p className="App-intro">
-          <button type="button" className="btn btn-primary" onClick={this.goToEditProfile}>Go to Profile</button>
-          <button type="button" className="btn btn-danger" onClick={this.handleLogout}>Logout</button>
-        </p>
       </div>
     );
   }
 }
 
-export default withAuth(App);
+export default App;
