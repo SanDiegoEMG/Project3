df862f70ce770a29885136f1c80f61329c12705d
isao92 <01agustin92@gmail.com>
2019-02-03 12:05:58 -0800

    bringing up to date for a new branch

--- client/src/components/MushroomPage.js
+++ client/src/components/MushroomPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import MushPage from "../pages/MushPage";
+import OurMushroom from "../pages/Ourmushroom.js";
 import '../App';
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
     console.log(process.env.REACT_APP_SECRET_CODE);
     return (
       <div className="App">
-        <MushPage />
+        <OurMushroom />
       </div>
     );
   }
