69c7d9dfb1f1da30704bc58c1bc03b0c1d694809
SD_emg <sandiegoemg@gmail.com>
2019-02-05 13:33:33 -0800

    going for clean merge

--- client/src/pages/MushPage.js
+++ client/src/pages/MushPage.js
@@ -11,16 +11,10 @@ class MushPage extends Component {
     super()
     this.state = {
       speciesArray: [],
-<<<<<<< HEAD
       description: "Get more info by clicking on a mushroom",
       grows_on: "",
       recipes: [],
       fun_facts: [],
-=======
-      click: true,
-      description: "Get more info by click on a mushroom",
-      gestation: "Gestation"
->>>>>>> 8b4f75c739334f75ff4f3a297fc8a4d9fe6c4936
   }
   };
 
@@ -32,17 +26,12 @@ class MushPage extends Component {
   showSpecies = () => {
     API.getAllSpecies()
       .then(res => {
-<<<<<<< HEAD
         this.setState({ speciesArray: res.data});
         console.log(this.state.speciesArray)
-=======
-        this.setState({ speciesArray: res.data, });
->>>>>>> 8b4f75c739334f75ff4f3a297fc8a4d9fe6c4936
       })
       .catch(err => console.log(err));
   };
 
-<<<<<<< HEAD
   getMushroomInfo = (description, recipes, fun_facts, grows_on) => {
     this.setState({
       description: description,
@@ -61,21 +50,10 @@ class MushPage extends Component {
 
   render() {
 
-=======
-  getMushroomInfo = (description, gestation) => {
-    this.setState({description: description, gestation: gestation});
-  };
-
-  render() {
->>>>>>> 8b4f75c739334f75ff4f3a297fc8a4d9fe6c4936
     return (
       <div className="Mushroompage">
       <Grid container spacing={16} style={{width: '100%', backgroundColor:"FEF2E4"}}>
         <Grid container spacing={24} style={{width: '100%', marginTop: "12%"}}>
-<<<<<<< HEAD
-=======
-          
->>>>>>> 8b4f75c739334f75ff4f3a297fc8a4d9fe6c4936
           <Grid item xs={3} style={{marginTop: 100}}>
             {/* glossary sidebar */}
             <ControlledExpansionPanels />
@@ -84,11 +62,7 @@ class MushPage extends Component {
           <Grid item xs={9} style={{width:"100%"}}>
             
               <Grid container spacing={8} style={{width:"100%"}} direcion="row" justify="center" alignItems="center">
-<<<<<<< HEAD
                   
-=======
-   
->>>>>>> 8b4f75c739334f75ff4f3a297fc8a4d9fe6c4936
                   {this.state.speciesArray.map(item => (
                     <Grid item xs={3} key={item._id}>
                     <MediaCard 
@@ -96,13 +70,9 @@ class MushPage extends Component {
                       name_latin={item.name_latin}
                       picsrc = {item.imageUrl}
                       description = {item.description}
-<<<<<<< HEAD
                       grows_on = {item.grows_on}
                       // recipes = {item.recipes}
                       fun_facts = {item.fun_facts}
-=======
-                      gestation = {item.gestation}
->>>>>>> 8b4f75c739334f75ff4f3a297fc8a4d9fe6c4936
                       getMushroomInfo={this.getMushroomInfo}
                       recipes= {this.arrayInArray(item)}
                     />
@@ -112,18 +82,9 @@ class MushPage extends Component {
 
                   <Grid item xs={3}></Grid>
                   <Grid item xs={6} style={{marginTop: 20}}>
-<<<<<<< HEAD
                     {this.state.description}
                     {this.state.grows_on}
                
-=======
-                  <p>{this.state.description}</p>
-                  <p>{this.state.gestation}</p>
-                    
-                      {/* <MainCard
-                      src={this.state.currentSrc}
-                      ></MainCard> */}
->>>>>>> 8b4f75c739334f75ff4f3a297fc8a4d9fe6c4936
                   </Grid>
                   <Grid item xs={2}></Grid>
               </Grid>
